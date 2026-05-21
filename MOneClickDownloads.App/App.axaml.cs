4872a6c8c5d06c0fa79099b2a30d4861829d9d54
Nan_Ting <a3380991070@163.com>
2026-05-21 12:41:46 +0800

    create proj

---
+++ MOneClickDownloads.App/App.axaml.cs
@@ -0,0 +1,31 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
+using Avalonia.Markup.Xaml;
+using MOneClickDownloads.App.ViewModels;
+using MOneClickDownloads.App.Views;
+using System.Linq;
+
+namespace MOneClickDownloads.App
+{
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ MOneClickDownloads.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MOneClickDownloads.App.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
